dacd52432e569bbdaf9618d764f009869a3afac2
Lucas Teixeira <38733166+lucasjct@users.noreply.github.com>
2020-01-21 01:35:48 -0300

    Create tags.spec.js
    create-search-tags

---
+++ tags.spec.js
@@ -0,0 +1,36 @@
+describe("DOM Elements", () => {
+
+beforeEach ( function () {
+    cy.visit("https://www.w3schools.com/")
+})
+
+    it('Test Title', () => {
+        cy.title()
+          .should('contain','W3Schools Online Web Tutorials')
+    })
+
+    it("Resource element", () =>{
+
+        cy.get("td").find('#gs_tti50')
+    })
+    it("Find Charset", () => {
+
+        
+        cy.get('meta[charset="UTF-8"]') //I can to do all tag way 
+    })
+    
+    it("Change Page",() => {
+
+        cy.contains("PYTHON").click({force:true})
+       
+    })
+    
+    it("Found analytics",() =>{
+
+        cy.get('script[src="https://www.google-analytics.com/analytics.js"]')
+    })
+
+    //it("Contains Google Ads?",() =>{
+
+    //   cy.get(".c imageholder")
+   // })
